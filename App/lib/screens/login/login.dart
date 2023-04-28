87bab18b1d8e80072fdd1f35ca43e9b9612c89ba
정혜수 <issuecap8@gmail.com>
2023-04-28 10:50:17 +0900

    Feat: login API 통신 test

--- App/lib/screens/login/login.dart
+++ App/lib/screens/login/login.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void fetchData() async {
+  final response  = await http.get('url들어갈 자리');
+
+  if (response.statusCode == 200) {
+    print('Response body: ${response.body}');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+}
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -30,7 +41,7 @@ class Login extends StatelessWidget {
                 child: GestureDetector(
                   onTap: (){
                     //이미지 클릭시 실행할 코드
-
+                    fetchData();
                   },
                   child: Image.asset('assets/images/kakao_login_medium_wide.png'),
                 ),
