4d25a643a4b9c5daf1c6fb2141e28a7b9203a279
원채령 <fairy3530@naver.com>
2023-04-28 14:49:35 +0900

    Feat : ios 위치 권한 허가

--- App/lib/screens/mypage/mypage.dart
+++ App/lib/screens/mypage/mypage.dart
@@ -218,7 +218,7 @@ class _MypageState extends State<Mypage> {
           ),
         ),
       ),
-      // bottomNavigationBar:  MapSample(),
+      bottomNavigationBar:  MapSample(),
     );
 
   }
--- App/lib/utils/map.dart
+++ App/lib/utils/map.dart
@@ -18,37 +18,8 @@ class MapSampleState extends State<MapSample> {
   PermissionStatus _permissionGranted = PermissionStatus.denied;
   LocationData? _locationData;
 
-  @override
-  void initState() {
-    super.initState();
-    getLocation();
-  }
 
-  void getLocation() async {
-    final location = Location();
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
 
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    _locationData = await location.getLocation();
-    setState(() {});
-    var lat = _locationData?.latitude ?? 0;
-    var long = _locationData?.longitude ?? 0;
-    LatLng my_location = _locationData != null ? LatLng(lat, long) : LatLng(37.42796133580664, -122.085749655962);
-
-  }
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
@@ -63,30 +34,78 @@ class MapSampleState extends State<MapSample> {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+
   @override
   Widget build(BuildContext context) {
-    final cameraPosition = CameraPosition(target: LatLng(_locationData!.latitude!, _locationData!.longitude!), zoom: 14.4746);
     // final cameraPosition = _locationData != null
     //     ? CameraPosition(target: LatLng(_locationData!.latitude!, _locationData!.longitude!), zoom: 14.4746)
     //     : _kGooglePlex;
+
+    void getLocation() async {
+      final location = Location();
+      _serviceEnabled = await location.serviceEnabled();
+      if (!_serviceEnabled) {
+        _serviceEnabled = await location.requestService();
+        if (!_serviceEnabled) {
+          return;
+        }
+      }
+
+      _permissionGranted = await location.hasPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        _permissionGranted = await location.requestPermission();
+        if (_permissionGranted != PermissionStatus.granted) {
+          return;
+        }
+      }
+
+      _locationData = await location.getLocation();
+      setState(() {});
+      var lat = _locationData?.latitude ?? 0;
+      var long = _locationData?.longitude ?? 0;
+    }
+
+    void _currentLocation() async {
+      final GoogleMapController controller = await _controller.future;
+      LocationData? currentLocation;
+      var location = new Location();
+      try {
+        currentLocation = await location.getLocation();
+      } on Exception {
+        currentLocation = null;
+      }
+      LatLng target = LatLng(
+        currentLocation?.latitude ?? 35.2051965,
+        currentLocation?.longitude ?? 126.8117383,
+      );
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 0,
+          target: target,
+          zoom: 80.0,
+        ),
+      ));
+    }
+
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: cameraPosition,
+        initialCameraPosition: CameraPosition(
+          bearing: 0,
+          target: LatLng(35.2051965, 126.8117383),
+          zoom: 14.0,
+        ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
+        onPressed: () {
+          _currentLocation();
+        },
+        label: const Text('내 위치로!'),
         icon: const Icon(Icons.directions_boat),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
