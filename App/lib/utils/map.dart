7b7604ed52f2e66a42987092d66b6dd1a230390c
최보영 <cjswp3550@naver.com>
2023-04-28 15:30:17 +0900

    Feat: 구글 맵 스타일 커스텀 구현

--- App/lib/utils/map.dart
+++ App/lib/utils/map.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'package:location/location.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapSample extends StatefulWidget {
   const MapSample({Key? key}) : super(key: key);
@@ -10,6 +10,10 @@ class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
 }
+  Future<String> loadMapStyle() async {
+    return await rootBundle.loadString('assets/style/map_style.txt');
+  }
+
 
 class MapSampleState extends State<MapSample> {
   Location location = new Location();
@@ -23,16 +27,17 @@ class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  var customMapStyle;
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    super.initState();
+    loadMapStyle().then((value) {
+      setState(() {
+        customMapStyle = value;
+      });
+    });
+  }
 
 
   @override
@@ -89,7 +94,7 @@ class MapSampleState extends State<MapSample> {
 
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: CameraPosition(
           bearing: 0,
           target: LatLng(35.2051965, 126.8117383),
@@ -97,6 +102,7 @@ class MapSampleState extends State<MapSample> {
         ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+          controller.setMapStyle(customMapStyle);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
