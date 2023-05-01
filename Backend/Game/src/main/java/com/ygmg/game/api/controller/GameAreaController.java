4d850acd951fe493b377e26bf4b66d7c5ba08e81
문태호 <ansxogh97@gmail.com>
2023-05-01 10:03:45 +0900

    Feat : SSE 구현

--- Backend/Game/src/main/java/com/ygmg/game/api/controller/GameAreaController.java
+++ Backend/Game/src/main/java/com/ygmg/game/api/controller/GameAreaController.java
@@ -8,20 +8,43 @@ import com.ygmg.game.db.model.Area;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @RestController
 @RequestMapping("/api/game/area")
 public class GameAreaController {
+
+    // SSEEmitter를 저장할 ConcurrentLinkedQueue
+    private final ConcurrentLinkedQueue<SseEmitter> emitters = new ConcurrentLinkedQueue<>();
+
     private final GameAreaService areaService;
     public GameAreaController(GameAreaService areaService){
         this.areaService = areaService;
     }
+
     @PostMapping("/")
     public ResponseEntity<String> createArea(@RequestBody AreaRegisterPostReq areaInfo) throws Exception {
+
         areaService.createArea(areaInfo);
+//
+//        면적이 생성되면  모든 클라이언트에게 데이터베이스에서 값을 가져와 랭킹을 매겨 전달합니다.
+//        랭킹 데이터를 가져옵니다. -> Rankings
+//        for (SseEmitter emitter : emitters) {
+//            try {
+//                // SSEEmitter로 데이터를 전달합니다.
+//                emitter.send(Rankings);
+//            } catch (IOException e) {
+//                // 에러 처리 로직을 작성합니다.
+//            }
+//        }
+//    }
+
+
         return ResponseEntity.status(200).body("면적이 생성되었습니다.");
     }
 //  해당 면적 ID에 해당하는 면적의 넓이 수정
@@ -54,4 +77,22 @@ public class GameAreaController {
         List<AreaRes> areas = areaService.getAreaByMemberIdAndAreaDate(memberId, areaDate);
         return ResponseEntity.status(200).body(areas);
     }
+
+    // SSE를 구현하는 API
+    @GetMapping("/connection")
+    public SseEmitter subscribe() {
+        // SSEEmitter를 생성합니다.
+        SseEmitter emitter = new SseEmitter();
+
+        // 생성한 SSEEmitter를 ConcurrentLinkedQueue에 저장합니다.
+        emitters.add(emitter);
+
+        // SSEEmitter의 onCompletion과 onTimeout 메소드를 사용하여 SSEEmitter를 제거합니다.
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+
+        // SSEEmitter를 반환합니다.
+        return emitter;
+    }
+
 }
