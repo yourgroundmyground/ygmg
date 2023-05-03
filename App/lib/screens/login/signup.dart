7a78522e097f5c81249f5e31721c07b9d2c3b7e8
정혜수 <issuecap8@gmail.com>
2023-05-03 17:42:53 +0900

    Feat: SignupScreen API 연동 코드 추가

--- App/lib/screens/login/signup.dart
+++ App/lib/screens/login/signup.dart
@@ -1,7 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class SignUpScreen extends StatelessWidget {
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  File? _image;
+  final picker = ImagePicker();
+  final TextEditingController _nicknameController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+
+  Future selectImage() async {
+    final pickedProfileImage = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedProfileImage != null) {
+        _image = File(pickedProfileImage.path);
+      } else {
+        print('프로필 이미지를 선택해주세요');
+      }
+    });
+  }
+
+  Future uploadData() async {
+    String url = "https://your-server-endpoint";
+    Dio dio = Dio();
+
+    FormData formData = FormData.fromMap({
+      "file": await MultipartFile.fromFile(_image!.path),
+      "nickname": _nicknameController.text,
+      "weight": _weightController.text,
+      // Add other fields if needed
+    });
+
+    try {
+      var response = await dio.post(url, data: formData);
+      print('성공');
+      print(response);
+    } catch (e) {
+      print('에러');
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +72,50 @@ class SignUpScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  const SizedBox(height: 100),
                   GestureDetector(
-                    onTap: (){
-                    },
-                    child: Image.asset('assets/images/addprofile.png'),
+                    onTap: selectImage,
+                    child: Container(
+                      height: 167,
+                      width: 167,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 1.0,
+                        ),
+                        boxShadow: [BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset: Offset(0,3)
+
+                        )]
+                      ),
+                      child: _image == null
+                        ? Container(
+                        width: 167,
+                        height: 167,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.white,
+                        ),
+                        child:Image.asset('assets/images/plus.png')
+                      )
+                        : ClipOval(
+                        child: Image.file(
+                        _image!,
+                        fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                  Text('닉네임'),
-                  SizedBox(
-                      child: Container(
+                  const SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('닉네임'),
+                      Container(
                         width: mediaWidth*0.85,
                         decoration: BoxDecoration(
                             boxShadow: [
@@ -48,6 +132,7 @@ class SignUpScreen extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             NicknameField(
+                              controller: _nicknameController,
                               hintText: '닉네임을 입력하세요',
                             ),
                             ElevatedButton(
@@ -55,15 +140,50 @@ class SignUpScreen extends StatelessWidget {
                                 child: Text('중복확인'))
                           ],
                         ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text('몸무게'),
+                      Container(
+                        width: mediaWidth*0.45,
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  blurRadius: 5,
+                                  offset: Offset(0,3)
+                              )
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            WeightField(
+                              controller: _weightController,
+                              hintText: '몸무게를 입력하세요',
+                            ),
+                            Text('kg'),
+                          ],
+                        ),
                       )
+                    ],
                   ),
-                  ElevatedButton(
-                      onPressed: (){},
-                      child: Text('확인', style: TextStyle(color: Colors.black)),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFFFDA65),
-                          elevation: 8
-                      )
+                  const SizedBox(height: 80),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                        onPressed: (){
+                          uploadData();
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFFFDA65),
+                            elevation: 8
+                        ),
+                        child: Text('확인', style: TextStyle(color: Colors.black)),
+                    )
+                  ]
                   )
                 ]
             ),
@@ -74,11 +194,15 @@ class SignUpScreen extends StatelessWidget {
   }
 }
 
+
+
 class NicknameField extends StatelessWidget {
+  final TextEditingController? controller;
   final String? hintText;
   final String? errorText;
 
   const NicknameField({
+    this.controller,
     this.hintText,
     this.errorText,
     Key? key}) : super(key: key);
@@ -98,6 +222,7 @@ class NicknameField extends StatelessWidget {
         SizedBox(
           width: mediaWidth*0.6,
           child: TextFormField(
+            controller: controller,
             cursorColor: Colors.teal,
             decoration: InputDecoration(
               contentPadding: EdgeInsets.all(10),
@@ -114,4 +239,50 @@ class NicknameField extends StatelessWidget {
     );
   }
 }
+
+class WeightField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? hintText;
+  final String? errorText;
+
+  const WeightField({
+    this.controller,
+    this.hintText,
+    this.errorText,
+    Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaWidth = MediaQuery.of(context).size.width;
+    final baseBorder = UnderlineInputBorder(
+        borderSide: BorderSide(
+            color: Colors.black,
+            width: 1.0
+        )
+    );
+
+    return Row(
+      children: [
+        SizedBox(
+          width: mediaWidth*0.4,
+          child: TextFormField(
+            controller: controller,
+            cursorColor: Colors.teal,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10),
+              hintText: hintText,
+              errorText: errorText,
+              hintStyle: TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
+              border: baseBorder,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
 
