b7582b7e473fda98b0672c7336ba2bc6d43b0999
원채령 <fairy3530@naver.com>
2023-05-03 17:45:36 +0900

    Fix: 차트 툴팁 추가(진행중)

--- App/lib/widgets/chart.dart
+++ App/lib/widgets/chart.dart
@@ -13,6 +13,7 @@ class BarChartSample4 extends StatefulWidget {
 }
 
 class BarChartSample4State extends State<BarChartSample4> {
+  int touchedIndex = -1;
   Widget bottomTitles(double value, TitleMeta meta) {
     const style = TextStyle(fontSize: 10);
     String text;
@@ -45,6 +46,7 @@ class BarChartSample4State extends State<BarChartSample4> {
 
   @override
   Widget build(BuildContext context) {
+
     return AspectRatio(
       aspectRatio: 1.2,
       child: Padding(
@@ -57,7 +59,68 @@ class BarChartSample4State extends State<BarChartSample4> {
               BarChartData(
                 alignment: BarChartAlignment.center,
                 barTouchData: BarTouchData(
-                  enabled: false,
+                  touchTooltipData: BarTouchTooltipData(
+                    tooltipBgColor: Colors.blueGrey,
+                    tooltipHorizontalAlignment: FLHorizontalAlignment.right,
+                    tooltipMargin: -10,
+                    getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                      String weekDay;
+                      switch (group.x) {
+                        case 0:
+                          weekDay = 'Monday';
+                          break;
+                        case 1:
+                          weekDay = 'Tuesday';
+                          break;
+                        case 2:
+                          weekDay = 'Wednesday';
+                          break;
+                        case 3:
+                          weekDay = 'Thursday';
+                          break;
+                        case 4:
+                          weekDay = 'Friday';
+                          break;
+                        case 5:
+                          weekDay = 'Saturday';
+                          break;
+                        case 6:
+                          weekDay = 'Sunday';
+                          break;
+                        default:
+                          throw Error();
+                      }
+                      return BarTooltipItem(
+                        '$weekDay\n',
+                        const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: (rod.toY - 1).toString(),
+                            style: TextStyle(
+                              color: YGMG_YELLOW,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  touchCallback: (FlTouchEvent event, barTouchResponse) {
+                    setState(() {
+                      if (!event.isInterestedForInteractions ||
+                          barTouchResponse == null ||
+                          barTouchResponse.spot == null) {
+                        touchedIndex = -1;
+                        return;
+                      }
+                      touchedIndex = barTouchResponse.spot!.touchedBarGroupIndex;
+                    });
+                  },
                 ),
                 titlesData: FlTitlesData(
                   show: true,
