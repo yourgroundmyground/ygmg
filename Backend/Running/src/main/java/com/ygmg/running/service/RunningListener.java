e2bbd6c0f4733c5489d6cac4cc9146a9fc83d496
문태호 <ansxogh97@gmail.com>
2023-05-04 09:24:29 +0900

    Feat : add RunningService Method in EventListener

--- Backend/Running/src/main/java/com/ygmg/running/service/RunningListener.java
+++ Backend/Running/src/main/java/com/ygmg/running/service/RunningListener.java
@@ -3,6 +3,7 @@ package com.ygmg.running.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ygmg.running.dto.RunningRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,13 +11,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class RunningListener {
 
+    private final RunningService runningService;
     ObjectMapper objectMapper = new ObjectMapper();
     @RabbitListener(queues = "ygmg.eventqueue")
     public void receiveMessage(@Payload String message) throws JsonProcessingException {
         RunningRequest runningRequest = objectMapper.readValue(message, RunningRequest.class);
-        log.info("dfdfd" + runningRequest.getMemberId());
+        log.info("message.memberID : " + runningRequest.getMemberId());
+        runningService.saveRunningRecord(runningRequest);
     }
 
 }
