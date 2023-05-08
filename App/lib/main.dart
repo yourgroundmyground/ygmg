b553255a194e3aa8577f713d6f27b3b8c90684cd
정혜수 <issuecap8@gmail.com>
2023-05-08 17:05:47 +0900

    Feat: riverpod Provider 상태관리 기능 추가

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app/screens/login/login.dart';
 import 'package:app/widgets/bottomnavbar.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
@@ -11,7 +12,11 @@ void main() async {
     nativeAppKey: 'bc0af151df3f9f3634ed6376aa5f3866',
     javaScriptAppKey: 'c59918aaed79bd379c5c09f3fc61be93',
   );
-  runApp(MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget{
