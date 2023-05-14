bb391ce09dbaa249adaba706be4c537cbdd719a0
원채령 <fairy3530@naver.com>
2023-05-15 01:43:25 +0900

    Feat : post 요청, marker 만들기(진행중)

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:app/screens/game/ingame.dart';
 
 void main() async {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
@@ -61,6 +62,7 @@ class MyApp extends StatelessWidget{
         primaryColor: Colors.white,
       ),
       home: LoadingScreen(),
+      // home: InGame(),
     );
   }
 }
