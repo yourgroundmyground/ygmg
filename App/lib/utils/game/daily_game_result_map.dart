a8870f0c1422ed117d67b972870bf91ff82eb7f4
최보영 <cjswp3550@naver.com>
2023-05-15 08:56:39 +0900

    Feat: 일별게임종료 지도 api 연결

--- App/lib/utils/game/daily_game_result_map.dart
+++ App/lib/utils/game/daily_game_result_map.dart
@@ -1,15 +1,18 @@
 import 'dart:math';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:intl/intl.dart';
 import 'dart:async';
-
 import '../../const/colors.dart';
 
 class DailyGameResultMap extends StatefulWidget {
-  const DailyGameResultMap({Key? key}) : super(key: key);
+  final int? memberId;
+
+  const DailyGameResultMap({
+    required this.memberId,
+    Key? key}) : super(key: key);
 
   @override
   State<DailyGameResultMap> createState() => _DailyGameResultMapState();
@@ -25,7 +28,7 @@ class _DailyGameResultMapState extends State<DailyGameResultMap> {
 
   Set<Polygon>_polygon = {};
   int polygonId = 0;
-  LatLng _center = LatLng(35.2051965, 126.8117383);
+  var _center;
 
   // colors.dart에서 정의한 색상 리스트
   final List<Color> _polygonColors = [
@@ -39,25 +42,74 @@ class _DailyGameResultMapState extends State<DailyGameResultMap> {
     YGMG_PURPLE,
   ];
 
+  // 폴리곤 좌표리스트로 변환
+  List<LatLng> extractPolygonCoordinates(List<dynamic> polygonData) {
+    List<LatLng> polygonCoordinates = [];
+    for (Map<String, dynamic> pointData in polygonData) {
+      double? lat = double.tryParse(pointData['areaCoordinateLat'].toString());
+      double? lng = double.tryParse(pointData['areaCoordinateLng'].toString());
+      if (lat != null && lng != null) {
+        LatLng coordinate = LatLng(lat, lng);
+        polygonCoordinates.add(coordinate);
+      }
+    }
+    return polygonCoordinates;
+  }
+
   // 일별 폴리곤 좌표리스트 조회 요청
   void getPolygonPoints() async {
     var dio = Dio();
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('yyyy-MM-dd').format(now);
     try {
       print('백에서 일별폴리곤 좌표리스트 가져오기!');
-      var response = await dio.get('............');     // *요청 API 주소 넣기
+      var response = await dio.get('http://k8c107.p.ssafy.io:8082/api/game/area/member/${widget.memberId}/$formattedDate');
+      // var response = await dio.get('http://k8c107.p.ssafy.io:8082/api/game/area/member/1/2023-05-13');     // *요청 API 주소 넣기
       print(response.data);
       // 데이터 형식
-      // {
-      //   "kakaoEmail": "suasdfa1@naver.com",
-      //   "memberBirth": "0512",
-      //   "memberName": "adf",
-      //   "memberNickname": "asdf",
-      //   "memberWeight": 23,
-      //   "profileImg": "https://asdfasdf.jpg"
-      // }
-
+      // List<Map<String, dynamic>> test = [
+      //     {
+      //         "areaId": 1,
+      //         "memberId": 1,
+      //         "areaDate": "2023-05-13 13:05:11",
+      //         "gameId": 1,
+      //         "areaSize": 5.691152769292121E-6,
+      //         "coordinateList": [
+      //             {
+      //                 "areaCoordinateId": 1,
+      //                 "areaCoordinateLat": 33.451690157220135,
+      //                 "areaCoordinateLng": 126.56738664637757,
+      //                 "areaCoordinateTime": "2023-05-12 23:13:05"
+      //             },
+      //             {
+      //                 "areaCoordinateId": 2,
+      //                 "areaCoordinateLat": 33.44949922860903,
+      //                 "areaCoordinateLng": 126.56737601368614,
+      //                 "areaCoordinateTime": "2023-05-12 23:13:05"
+      //             },
+      //             {
+      //                 "areaCoordinateId": 3,
+      //                 "areaCoordinateLat": 33.4502143361292,
+      //                 "areaCoordinateLng": 126.57104991347728,
+      //                 "areaCoordinateTime": "2023-05-12 23:13:05"
+      //             },
+      //             {
+      //                 "areaCoordinateId": 4,
+      //                 "areaCoordinateLat": 33.45126460137033,
+      //                 "areaCoordinateLng": 126.57070654854132,
+      //                 "areaCoordinateTime": "2023-05-12 23:13:05"
+      //             },
+      //             {
+      //                 "areaCoordinateId": 5,
+      //                 "areaCoordinateLat": 33.451690157220135,
+      //                 "areaCoordinateLng": 126.56738664637757,
+      //                 "areaCoordinateTime": "2023-05-12 23:13:05"
+      //             }
+      //         ]
+      //     },
+      // ];
       // 받은 좌표 리스트로 폴리곤의 위치 좌표 구하기
-      List<dynamic> polygonData = response.data;
+      List<dynamic> polygonData = response.data[0]['coordinateList'];
       List<LatLng> polygonCoordinates = extractPolygonCoordinates(polygonData);
 
       // 랜덤한 색상 선택
@@ -91,18 +143,6 @@ class _DailyGameResultMapState extends State<DailyGameResultMap> {
     }
   }
 
-  // 폴리곤 좌표리스트로 변환
-  List<LatLng> extractPolygonCoordinates(List<dynamic> polygonData) {
-   List<LatLng> polygonCoordinates = [];
-    for (dynamic pointData in polygonData) {
-      double lat = pointData['lat'];
-      double lng = pointData['long'];
-      LatLng coordinate = LatLng(lat, lng);
-      polygonCoordinates.add(coordinate);
-    }
-    return polygonCoordinates;
-  }
-
   @override
   void initState() {
     loadMapStyle().then((style) {
@@ -116,21 +156,22 @@ class _DailyGameResultMapState extends State<DailyGameResultMap> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-        bearing: 0,
-        target: _center,
-        zoom: 16.0,
-      ),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-        controller.setMapStyle(customMapStyle);
-      },
-      polygons: _polygon,
-      myLocationButtonEnabled: false,
-      rotateGesturesEnabled: false,
-      zoomControlsEnabled: false,
-      tiltGesturesEnabled: false,
-    );
+    return _center == null ? Center(child: CircularProgressIndicator(),) :
+      GoogleMap(
+        initialCameraPosition: CameraPosition(
+          bearing: 0,
+          target: _center,
+          zoom: 16.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          controller.setMapStyle(customMapStyle);
+        },
+        polygons: _polygon,
+        myLocationButtonEnabled: false,
+        rotateGesturesEnabled: false,
+        zoomControlsEnabled: false,
+        tiltGesturesEnabled: false,
+      );
   }
 }
