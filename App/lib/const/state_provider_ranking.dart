7ea2cbb015aa180b89bae152e5ba239a9ac37eff
정혜수 <issuecap8@gmail.com>
2023-05-16 14:11:16 +0900

    Feat: 랭킹 정보 상태관리 프로바이더 코드 수정

--- App/lib/const/state_provider_ranking.dart
+++ App/lib/const/state_provider_ranking.dart
@@ -59,9 +59,6 @@ class RankingInfoNotifier extends StateNotifier<List<RankingInfo>> {
     }
   }
 
-  // void updateRankingInfoList(List<RankingInfo> newRankingInfoList) {
-  //   state = newRankingInfoList;
-  // }
 }
 
 final rankingInfoProvider = StateNotifierProvider<RankingInfoNotifier, List<RankingInfo>>((ref)  => RankingInfoNotifier(ref));
@@ -70,64 +67,3 @@ final rankingInfoFutureProvider = FutureProvider<List<RankingInfo>>((ref) {
   return ref.read(rankingInfoProvider.notifier).fetchRankingDataAndSaveState();
 });
 
-//데이터 형식
-// [
-// {
-// "memberId": "1",
-// "areaSize": 10361.7
-// },
-// {
-// "memberId": "4",
-// "areaSize": 7361.7
-// },
-// {
-// "memberId": "5",
-// "areaSize": 361.7
-// }
-// ]
-
-// import 'package:dio/dio.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-//
-// class RankingInfo {
-//   final String memberId;
-//   final double areaSize;
-//
-//   RankingInfo({
-//     required this.memberId,
-//     required this.areaSize,
-//
-//   });
-// }
-//
-// class RankingInfoNotifier extends StateNotifier<List<RankingInfo>> {
-//   final Ref ref;
-//
-//   RankingInfoNotifier(this.ref): super([]);
-//
-//   Future<void> fetchRankingDataAndSaveState() async {
-//     try {
-//       // final response = await Dio().get('http://192.168.0.17:8082/api/game/ranking/top/');
-//       final response = await Dio().get('http://k8c107.p.ssafy.io/api/game/ranking/top/');
-//       final List<dynamic> data = response.data;
-//       print('페치 게임데이터 $data');
-//
-//       final rankingInfoList = data.map((item) {
-//         return RankingInfo(
-//             memberId: item['memberId'],
-//             areaSize: double.tryParse(item['areaSize'].toString()) ?? 0.0);
-//       }).toList();
-//
-//       state = rankingInfoList;
-//     } catch (error) {
-//       print('랭킹에러 $error');
-//     }
-//   }
-//
-// // void updateRankingInfoList(List<RankingInfo> newRankingInfoList) {
-// //   state = newRankingInfoList;
-// // }
-// }
-//
-// final rankingInfoProvider = StateNotifierProvider<RankingInfoNotifier, List<RankingInfo>>((ref)  => RankingInfoNotifier(ref));
-//
