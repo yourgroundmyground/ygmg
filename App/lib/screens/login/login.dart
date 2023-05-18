9b06c9dc9561fb7ffbd3e8e6098d4ffdc88d22f4
정혜수 <issuecap8@gmail.com>
2023-05-18 23:50:38 +0900

    Fix: 회원가입 오류로 인한 api주소 원복

--- App/lib/screens/login/login.dart
+++ App/lib/screens/login/login.dart
@@ -14,7 +14,7 @@ void fetchData(BuildContext context) async {
       // print('카카오톡으로 로그인 성공 ${token.accessToken}');
       sendCode(context, token.accessToken);
     }catch(error){
-      print('카카오톡으로 로그인 실패 $error');
+      // print('카카오톡으로 로그인 실패 $error');
 
       // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
       // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
@@ -49,7 +49,7 @@ void sendCode(BuildContext context, var accessToken) async {
   // var body = json.encode(code);
 
     final response = await dio.post(
-      "https://xofp5xphrk.execute-api.ap-northeast-2.amazonaws.com/ygmg/api/member/kakao",
+      "http://k8c107.p.ssafy.io/api/member/kakao",
       options: Options(
           headers: {"Content-Type": "application/json"}),
       data: jsonEncode(code),
